36edf8e646bb381cdbde6a2f79712de9888e3e25
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-09-03 19:31:04 +1000

    npm init setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let defaultEnv = {
+    PORT: 3030,
+    SOME_DATABASE_URL: "blah blah blah",
+    SECURITY_TOKEN: "token",
+    SECRET_API_KEY: "tis' a secret",
+    JWT_SECRET_KEY: "tis' also a secret"
+}
+
+console.log(defaultEnv); 
